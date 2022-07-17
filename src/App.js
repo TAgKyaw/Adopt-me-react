9b90d75bfa1ef60134faa7bb8a7a73ae66df68c2
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-17 17:27:08 +0630

    Changed App.js to JSX expressions.

--- src/App.js
+++ src/App.js
@@ -3,28 +3,36 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Buggs" animal="Dog" breed="Retriever" />
+      <Pet name="Kinko" animal="Cat" breed="Mixed" />
+    </div>
+  );
   // creating a html element from react called a component.
   // React.createElement("elementName",{element_attributes},"[array of children elements] OR child1, child2")
-  return React.createElement(
-    "div",
-    {
-      id: "something important",
-    },
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Mutt" }),
-      React.createElement(Pet, {
-        name: "Buggs",
-        animal: "Dog",
-        breed: "Retriever",
-      }),
-      React.createElement(Pet, {
-        name: "Kinko",
-        animal: "Cat",
-        breed: "Mixed",
-      }),
-    ]
-  );
+  // return React.createElement(
+  //   "div",
+  //   {
+  //     id: "something important",
+  //   },
+  //   [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Mutt" }),
+  //     React.createElement(Pet, {
+  //       name: "Buggs",
+  //       animal: "Dog",
+  //       breed: "Retriever",
+  //     }),
+  //     React.createElement(Pet, {
+  //       name: "Kinko",
+  //       animal: "Cat",
+  //       breed: "Mixed",
+  //     }),
+  //   ]
+  // );
 };
 // Rendering components from App container into the document
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -4,9 +4,10 @@ export default function Pet({ name, animal, breed }) {
   // this is a stamp/ a component
   // This is JSX - javascript react
   //Javascript can return only 1 element therefore all elements are wrapped in <div>
+  const hello = 2 / 2 == 1 ? "Hello" : null;
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{hello + " " + name.toUpperCase()}</h1>
       <h2>{animal}</h2>
       <h2>{breed}</h2>
     </div>
