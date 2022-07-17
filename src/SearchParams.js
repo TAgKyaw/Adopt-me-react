afb8885f3cd544e20bcf8738dd428ac0d17913be
Than Aung Kyaw <dannynaing727@gmail.com>
2022-07-17 18:36:13 +0630

    created custom hook, dropDown for animal and breed

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -2,14 +2,17 @@ import React from "react";
 import { useState } from "react";
 // New API Library - if haven't installed, parcel will fetch it for us
 import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   //   const location = "Seattle, WA";
   const [location, setLocation] = useState("Seattle, WA");
   //this is a Hook with a default state/first state
-  const [animal, setAnimal] = useState("Choose");
-  const [breed, setBreed] = useState("Choose");
+  //   const [animal, setAnimal] = useState("Choose");
+  //   const [breed, setBreed] = useState("Choose");
   const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, breedDropdown] = useDropdown("Breed", "", breeds);
 
   return (
     // We use className instead of class because class is reserved in JavaScript. Cannot read class as an attribute.
@@ -25,41 +28,8 @@ const SearchParams = () => {
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
-        <label htmlFor="animal">
-          Animal
-          <select
-            id="animal"
-            value={animal}
-            onChange={(event) => setAnimal(event.target.value)}
-            onBlur={(event) => setAnimal(event.target.value)}
-          >
-            <option value="">Choose</option>
-            {/* Implicit return  */}
-            {ANIMALS.map((animal) => (
-              <option key={animal} value={animal}>
-                {animal}
-              </option>
-            ))}
-          </select>
-        </label>
-        <label htmlFor="animal">
-          Breed
-          <select
-            id="breed"
-            value={breed}
-            onChange={(event) => setBreed(event.target.value)}
-            onBlur={(event) => setBreed(event.target.value)}
-            disabled={breeds.length === 0}
-          >
-            <option value="">Choose</option>
-            {/* Implicit return  */}
-            {breeds.map((breedString) => (
-              <option key={breedString} value={breedString}>
-                {breedString}
-              </option>
-            ))}
-          </select>
-        </label>
+        <AnimalDropdown />
+        <breedDropdown />
         <button type="submit">Submit</button>
       </form>
     </div>
